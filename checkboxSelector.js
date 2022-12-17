9ca3cb60a0d281d38a2a7333b1d8fc2879bdd33c
Martin Eder <med@imap.cc>
2022-12-17 16:22:17 +0100

    replaced for loop with arrow function and various improvements

--- checkboxSelector.js
+++ checkboxSelector.js
@@ -7,11 +7,17 @@ let shiftDown = false;
 // the rectangle to show while dragging
 var rectangle = undefined;
 
-// coordinates of the rectangle
-let startX = 0;
-let startY = 0;
-let endX = 0;
-let endY = 0;
+// rectangle object to draw the box upon dragging
+let rec = {
+  startX: 0,
+  startY: 0,
+  endX: 0,
+  endY: 0,
+  style: 'position:absolute;border: 2px dotted black;background: white; opacity:0.5;z-index:100;font-size: 0px'
+}
+
+
+//////// Event Listeners ////////
 
 document.addEventListener('keydown', e => {
   // enable dragging if shift key is pressed
@@ -29,10 +35,8 @@ document.addEventListener('mousedown', e => {
   // start dragging-mode if shift key is pressed
   if (shiftDown) {
     isDragging = true;
-    startX = e.pageX;
-    startY = e.pageY;
-    endX = startX;
-    endY = startY;
+    rec.startX = rec.endX = e.pageX;
+    rec.startY = rec.endY = e.pageY;
     e.preventDefault();
   }
 });
@@ -41,14 +45,11 @@ document.addEventListener('mousemove', e => {
   // show dragging rectangle is drag mode is activated
   if (isDragging) {
     rectangle.style.visibility = 'visible';
-    endX = e.pageX;
-    endY = e.pageY;
+    rec.endX = e.pageX;
+    rec.endY = e.pageY;
     e.preventDefault();
+    drawRectangle();
 
-    rectangle.style.left = `${startX}px`;
-    rectangle.style.top = `${startY}px`;
-    rectangle.style.width = `${endX - startX}px`;
-    rectangle.style.height = `${endY - startY}px`;
   }
 });
 
@@ -61,25 +62,34 @@ document.addEventListener('mouseup', () => {
   }
 });
 
+
+//////// Functions ////////
+
+function drawRectangle() {
+  rectangle.style.left = `${rec.startX}px`;
+  rectangle.style.top = `${rec.startY}px`;
+  rectangle.style.width = `${rec.endX - rec.startX}px`;
+  rectangle.style.height = `${rec.endY - rec.startY}px`;
+}
+
 function checkCheckboxes() {
     var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-
-    for (var i = 0; i < checkboxes.length; i++) {
-        posObject = checkboxes[i].getBoundingClientRect();
-        if ((endY >= posObject.top && posObject.top >= startY || endY <= posObject.top && posObject.top <= startY) &&
-            (endY >= posObject.bottom && posObject.bottom >= startY || endY <= posObject.bottom && posObject.bottom <= startY) &&
-            (endX >= posObject.left && posObject.left >= startX || endX <= posObject.left && posObject.left <= startX) &&
-            (endX >= posObject.right && posObject.right >= startX || endX <= posObject.right && posObject.right <= startX)) {
-              // toggle status of the checkbox in range
-              checkboxes[i].checked ? checkboxes[i].checked = false : checkboxes[i].checked = true;
+    checkboxes.forEach(chckbx => {
+      posObject = chckbx.getBoundingClientRect();
+      if ((rec.endY >= posObject.top && posObject.top >= rec.startY || rec.endY <= posObject.top && posObject.top <= rec.startY) &&
+          (rec.endY >= posObject.bottom && posObject.bottom >= rec.startY || rec.endY <= posObject.bottom && posObject.bottom <= rec.startY) &&
+          (rec.endX >= posObject.left && posObject.left >= rec.startX || rec.endX <= posObject.left && posObject.left <= rec.startX) &&
+          (rec.endX >= posObject.right && posObject.right >= rec.startX || rec.endX <= posObject.right && posObject.right <= rec.startX)) {
+            // toggle status of the checkbox in range
+            chckbx.checked ? chckbx.checked = false : chckbx.checked = true;
         }
-    }
+    });
 } 
 
 function initMe() {
   // create div which is used for showing the rectangle and initialize it
   rectangle = document.createElement('div');
-  rectangle.style.cssText = 'position:absolute;border: 2px dotted black;background: white; opacity:0.5;z-index:100;font-size: 0px';
+  rectangle.style.cssText = rec.style;
   document.body.appendChild(rectangle);
 }
 
