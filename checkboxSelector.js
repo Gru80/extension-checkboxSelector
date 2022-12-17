715b4d9171041dedf844b323420da4d848a0db45
Martin Eder <med@imap.cc>
2022-12-17 15:43:26 +0100

    got rid of draw() function as it is not needed)

--- checkboxSelector.js
+++ checkboxSelector.js
@@ -14,16 +14,19 @@ let endX = 0;
 let endY = 0;
 
 document.addEventListener('keydown', e => {
+  // enable dragging if shift key is pressed
   if (e.shiftKey) {
     shiftDown = true;
   }
 });
 
 document.addEventListener('keyup', e => {
-    shiftDown = false;
+  // disable dragging mode upon (any) key is released  
+  shiftDown = false;
 });
 
 document.addEventListener('mousedown', e => {
+  // start dragging-mode if shift key is pressed
   if (shiftDown) {
     isDragging = true;
     startX = e.pageX;
@@ -35,35 +38,31 @@ document.addEventListener('mousedown', e => {
 });
 
 document.addEventListener('mousemove', e => {
+  // show dragging rectangle is drag mode is activated
   if (isDragging) {
     rectangle.style.visibility = 'visible';
     endX = e.pageX;
     endY = e.pageY;
     e.preventDefault();
+
+    rectangle.style.left = `${startX}px`;
+    rectangle.style.top = `${startY}px`;
+    rectangle.style.width = `${endX - startX}px`;
+    rectangle.style.height = `${endY - startY}px`;
   }
 });
 
 document.addEventListener('mouseup', () => {
+  // stop dragging mode if mouse button is released
   if (isDragging) {
     isDragging = false;
     rectangle.style.visibility = 'hidden';
     checkCheckboxes();
   }
-
 });
-
-function draw() {
-  rectangle.style.left = `${startX}px`;
-  rectangle.style.top = `${startY}px`;
-  rectangle.style.width = `${endX - startX}px`;
-  rectangle.style.height = `${endY - startY}px`;
-  requestAnimationFrame(draw);
-}
 
 function checkCheckboxes() {
     var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    var candidateCheckboxes = [];
-    var allChecked = true;
 
     for (var i = 0; i < checkboxes.length; i++) {
         posObject = checkboxes[i].getBoundingClientRect();
@@ -71,33 +70,17 @@ function checkCheckboxes() {
             (endY >= posObject.bottom && posObject.bottom >= startY || endY <= posObject.bottom && posObject.bottom <= startY) &&
             (endX >= posObject.left && posObject.left >= startX || endX <= posObject.left && posObject.left <= startX) &&
             (endX >= posObject.right && posObject.right >= startX || endX <= posObject.right && posObject.right <= startX)) {
-
-              if (checkboxes[i].checked === false) {
-                allChecked = false;
-            }
-            //add the checkbox to the list of inputs within the selection
-            candidateCheckboxes.push(checkboxes[i]);
-        }
-    }
-
-    //iterate through all checkbox inputs in the selection
-    for (var i = 0; i < candidateCheckboxes.length; i++) {
-        //if every checkbox in the selection is checked, uncheck all of them
-        if (allChecked) {
-            candidateCheckboxes[i].checked = false;
-        }
-        //otherwise check all boxes in selection
-        else {
-            candidateCheckboxes[i].checked = true;
+              // toggle status of the checkbox in range
+              checkboxes[i].checked ? checkboxes[i].checked = false : checkboxes[i].checked = true;
         }
     }
 } 
 
 function initMe() {
+  // create div which is used for showing the rectangle and initialize it
   rectangle = document.createElement('div');
   rectangle.style.cssText = 'position:absolute;border: 2px dotted black;background: white; opacity:0.5;z-index:100;font-size: 0px';
   document.body.appendChild(rectangle);
-  requestAnimationFrame(draw);
 }
 
 initMe(); 
