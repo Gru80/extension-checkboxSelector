4e6db8a151347d34979c8f9c4c93f172129c028e
Martin Eder <med@imap.cc>
2022-12-17 14:29:19 +0100

    1st working version with actual selection

--- checkboxSelector.js
+++ checkboxSelector.js
@@ -1,30 +1,54 @@
-//const rectangle = document.getElementById('rectangle');
+// is dragging active
+let isDragging = false;
 
-let isDrawing = false;
+// is shift key pressed
+let shiftDown = false;
+
+// the rectangle to show while dragging
+var rectangle = undefined;
+
+// coordinates of the rectangle
 let startX = 0;
 let startY = 0;
 let endX = 0;
 let endY = 0;
 
+document.addEventListener('keydown', e => {
+  if (e.shiftKey) {
+    shiftDown = true;
+  }
+});
+
+document.addEventListener('keyup', e => {
+    shiftDown = false;
+});
+
 document.addEventListener('mousedown', e => {
-  isDrawing = true;
-  startX = e.pageX;
-  startY = e.pageY;
-  endX = startX;
-  endY = startY;
+  if (shiftDown) {
+    isDragging = true;
+    startX = e.pageX;
+    startY = e.pageY;
+    endX = startX;
+    endY = startY;
+    e.preventDefault();
+  }
 });
 
 document.addEventListener('mousemove', e => {
-  if (isDrawing) {
+  if (isDragging) {
     rectangle.style.visibility = 'visible';
     endX = e.pageX;
     endY = e.pageY;
+    e.preventDefault();
   }
 });
 
 document.addEventListener('mouseup', () => {
-  isDrawing = false;
-  rectangle.style.visibility = 'hidden';
+  if (isDragging) {
+    isDragging = false;
+    rectangle.style.visibility = 'hidden';
+    checkCheckboxes();
+  }
 
 });
 
@@ -36,8 +60,44 @@ function draw() {
   requestAnimationFrame(draw);
 }
 
-var rectangle = document.createElement('div');
-rectangle.style.cssText = 'position:absolute;border: 2px dotted black;background: white; opacity:0.5;z-index:100;font-size: 0px';
-document.body.appendChild(rectangle);
+function checkCheckboxes() {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    var candidateCheckboxes = [];
+    var allChecked = true;
 
-requestAnimationFrame(draw);
+    for (var i = 0; i < checkboxes.length; i++) {
+        posObject = checkboxes[i].getBoundingClientRect();
+        if ((endY >= posObject.top && posObject.top >= startY || endY <= posObject.top && posObject.top <= startY) &&
+            (endY >= posObject.bottom && posObject.bottom >= startY || endY <= posObject.bottom && posObject.bottom <= startY) &&
+            (endX >= posObject.left && posObject.left >= startX || endX <= posObject.left && posObject.left <= startX) &&
+            (endX >= posObject.right && posObject.right >= startX || endX <= posObject.right && posObject.right <= startX)) {
+
+              if (checkboxes[i].checked === false) {
+                allChecked = false;
+            }
+            //add the checkbox to the list of inputs within the selection
+            candidateCheckboxes.push(checkboxes[i]);
+        }
+    }
+
+    //iterate through all checkbox inputs in the selection
+    for (var i = 0; i < candidateCheckboxes.length; i++) {
+        //if every checkbox in the selection is checked, uncheck all of them
+        if (allChecked) {
+            candidateCheckboxes[i].checked = false;
+        }
+        //otherwise check all boxes in selection
+        else {
+            candidateCheckboxes[i].checked = true;
+        }
+    }
+} 
+
+function initMe() {
+  rectangle = document.createElement('div');
+  rectangle.style.cssText = 'position:absolute;border: 2px dotted black;background: white; opacity:0.5;z-index:100;font-size: 0px';
+  document.body.appendChild(rectangle);
+  requestAnimationFrame(draw);
+}
+
+initMe(); 
