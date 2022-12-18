87bf482e00fd4538f14952c58ff526e851d8d338
Martin Eder <med@imap.cc>
2022-12-18 14:11:21 +0100

    selection rectangle now shown also if x or y move values are negative to start position

--- checkboxSelector.js
+++ checkboxSelector.js
@@ -7,8 +7,16 @@ let shiftDown = false;
 // the rectangle to show while dragging
 var rectangle = undefined;
 
-// rectangle object to draw the box upon dragging
+// rectangle object specify the selected range
 let rec = {
+  startX: 0,
+  startY: 0,
+  endX: 0,
+  endY: 0,
+}
+
+// rectangle object to draw the box upon dragging
+let recDraw = {
   startX: 0,
   startY: 0,
   endX: 0,
@@ -35,8 +43,8 @@ document.addEventListener('mousedown', e => {
   // start dragging-mode if shift key is pressed
   if (shiftDown) {
     isDragging = true;
-    rec.startX = rec.endX = e.pageX;
-    rec.startY = rec.endY = e.pageY;
+    rec.startX = rec.endX = recDraw.startX = recDraw.endX = e.pageX;
+    rec.startY = rec.endY = recDraw.startY = recDraw.endY = e.pageY;
     e.preventDefault();
   }
 });
@@ -45,17 +53,40 @@ document.addEventListener('mousemove', e => {
   // show dragging rectangle is drag mode is activated
   if (isDragging) {
     rectangle.style.visibility = 'visible';
-    rec.endX = e.pageX;
-    rec.endY = e.pageY;
     e.preventDefault();
-    drawRectangle();
 
+    // Take care that the rectangle is displayed also with negative x or y values
+    if (e.pageX >= rec.startX && e.pageY >= rec.startY) {
+      //console.log("right bottom")
+      recDraw.endX = e.pageX;
+      recDraw.endY = e.pageY;
+    } else if (e.pageX < rec.startX && e.pageY >= rec.startY) {
+      //console.log("left bottom - negative X")
+      recDraw.startX = e.pageX;
+      recDraw.endX = rec.startX;
+      recDraw.endY = e.pageY;
+    } else if (e.pageX >= rec.startX && e.pageY < rec.startY) {
+      //console.log("right top - negative Y")
+      recDraw.endY = rec.startY;
+      recDraw.startY = e.pageY;
+      recDraw.endX = e.pageX; 
+    } else if (e.pageX < rec.startX && e.pageY < rec.startY) {
+      //console.log("left top - negative X + negative Y")
+      recDraw.startY = e.pageY;
+      recDraw.startX = e.pageX;
+      recDraw.endX = rec.startX;
+      recDraw.endY = rec.startY;
+    } 
+
+    drawRectangle();
   }
 });
 
-document.addEventListener('mouseup', () => {
+document.addEventListener('mouseup', e => {
   // stop dragging mode if mouse button is released
   if (isDragging) {
+    rec.endX = e.pageX;
+    rec.endY = e.pageY;
     isDragging = false;
     rectangle.style.visibility = 'hidden';
     checkCheckboxes();
@@ -66,10 +97,10 @@ document.addEventListener('mouseup', () => {
 //////// Functions ////////
 
 function drawRectangle() {
-  rectangle.style.left = `${rec.startX}px`;
-  rectangle.style.top = `${rec.startY}px`;
-  rectangle.style.width = `${rec.endX - rec.startX}px`;
-  rectangle.style.height = `${rec.endY - rec.startY}px`;
+  rectangle.style.left = `${recDraw.startX}px`;
+  rectangle.style.top = `${recDraw.startY}px`;
+  rectangle.style.width = `${recDraw.endX - recDraw.startX}px`;
+  rectangle.style.height = `${recDraw.endY - recDraw.startY}px`;
 }
 
 function checkCheckboxes() {
@@ -86,11 +117,11 @@ function checkCheckboxes() {
     });
 } 
 
-function initMe() {
+function initCheckboxSelector() {
   // create div which is used for showing the rectangle and initialize it
   rectangle = document.createElement('div');
-  rectangle.style.cssText = rec.style;
+  rectangle.style.cssText = recDraw.style;
   document.body.appendChild(rectangle);
 }
 
-initMe(); 
+initCheckboxSelector(); 
